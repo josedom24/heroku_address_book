45928fc754e92f36ad34b9a9c3a62daf4dfce49d
José Domingo Muñoz Rodríguez <josedom24@gmail.com>
2019-03-11 19:48:00 +0100

    config bd

--- include/db.php
+++ include/db.php
@@ -1,9 +1,9 @@
 <?php
-	$con = mysql_connect("localhost","root","root");
+	$con = mysql_connect("eu-cdbr-west-02.cleardb.net","b3526bc9bdd43d","4e3bc5bd");
 	if($con==NULL){
 		echo "Error establishing database connection.";
 	}
 	else{
-		mysql_select_db("address-book");
+		mysql_select_db("heroku_b81fa0a6776fe38");
 	}
 ?>
