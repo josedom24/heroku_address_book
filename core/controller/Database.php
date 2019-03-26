cb9ef0549ed64068b95d697b30a71499973f0b00
José Domingo <josedom24@gmail.com>
2019-03-26 12:34:17 +0100

    New database.php

--- core/controller/Database.php
+++ core/controller/Database.php
@@ -3,12 +3,27 @@ class Database {
 	public static $db;
 	public static $con;
 	function Database(){
-		
-		
-		$this->user="";
-		$this->pass="";
-		$this->host="";
-		$this->ddbb="";
+		// Entorno de producción
+        if (isset($_ENV["CLEARDB_DATABASE_URL"])
+        {
+            $var_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
+            $var_server   = $cleardb_url["host"];
+            $var_username = $cleardb_url["user"];
+            $var_password = $cleardb_url["pass"];
+            $var_db       = substr($cleardb_url["path"],1);
+        }
+        else //Entorno de desarrollo
+        {
+            $var_server   = "localhost";
+            $var_username = "username";
+            $var_password = "userpassword";
+            $var_db       = "newdb";
+        }
+
+		$this->user=$var_username;
+		$this->pass=$var_password;
+		$this->host=$var_server;
+		$this->ddbb=$var_db;
 	}
 
 	function connect(){
--- core/controller/Database2.php
+++ core/controller/Database2.php
@@ -3,29 +3,12 @@ class Database {
 	public static $db;
 	public static $con;
 	function Database(){
-		// Entorno de producción
-        if (isset($_ENV["CLEARDB_DATABASE_URL"])
-        {
-            $var_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
-            $var_server   = $cleardb_url["host"];
-            $var_username = $cleardb_url["user"];
-            $var_password = $cleardb_url["pass"];
-            $var_db       = substr($cleardb_url["path"],1);
-        }
-        else //Entorno de desarrollo
-        {
-            $var_server   = "localhost";
-            $var_username = "username";
-            $var_password = "userpassword";
-            $var_db       = "newdb";
-        }
-
-
-		echo ($var_username);
-		$this->user=$var_username;
-		$this->pass=$var_password;
-		$this->host=$var_server;
-		$this->ddbb=$var_db;
+		
+		
+		$this->user="";
+		$this->pass="";
+		$this->host="";
+		$this->ddbb="";
 	}
 
 	function connect(){
